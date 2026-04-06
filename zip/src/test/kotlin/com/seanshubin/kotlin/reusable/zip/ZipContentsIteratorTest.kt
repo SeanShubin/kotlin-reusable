2e764681cd1303bb59e26cc7258824e24e4c0a58
Sean Shubin <sshubin@cj.com>
2026-04-06 16:20:52 -0700

    made ZipContentsIteratorTest self contained

--- zip/src/test/kotlin/com/seanshubin/kotlin/reusable/zip/ZipContentsIteratorTest.kt
+++ zip/src/test/kotlin/com/seanshubin/kotlin/reusable/zip/ZipContentsIteratorTest.kt
@@ -1,7 +1,11 @@
 package com.seanshubin.kotlin.reusable.zip
 
 import org.junit.Test
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 import java.nio.charset.StandardCharsets
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
 import kotlin.test.assertEquals
 
 class ZipContentsIteratorTest {
@@ -53,7 +57,8 @@ class ZipContentsIteratorTest {
         }
 
         val name = "data.zip"
-        val inputStream = javaClass.classLoader.getResourceAsStream(name)
+        val testData = createTestZipData()
+        val inputStream = ByteArrayInputStream(testData)
         val iterator: Iterator<ZipContents> =
             ZipContentsIterator(inputStream, name, ::isZip, ZipContentsIterator.AcceptAll)
 
@@ -63,4 +68,82 @@ class ZipContentsIteratorTest {
     }
 
     fun scrubLine(line: String): String = line.replace("\\\\", "/")
+
+    private fun createTestZipData(): ByteArray {
+        val zipC = createZip {
+            addDirectory("dir-c/")
+            addFile("dir-c/file-f.txt", "Hello F!")
+            addFile("dir-c/file-g.txt", "Hello G!")
+        }
+
+        val zipD = createZip {
+            addDirectory("dir-d/")
+            addFile("dir-d/file-h.txt", "Hello H!")
+        }
+
+        val zipE = createZip {
+            addDirectory("dir-e/")
+            addFile("dir-e/file-i.txt", "Hello I!")
+            addFile("dir-e/file-j.txt", "Hello J!")
+        }
+
+        val zipB = createZip {
+            addDirectory("dir-b/")
+            addFile("dir-b/file-d.txt", "Hello D!")
+            addFile("dir-b/file-e.txt", "Hello E!")
+            addZipFile("dir-b/zip-c.zip", zipC)
+            addZipFile("dir-b/zip-d.zip", zipD)
+            addZipFile("dir-b/zip-e.zip", zipE)
+        }
+
+        val zipF = createZip {
+            addDirectory("dir-f/")
+            addFile("dir-f/file-k.txt", "Hello K!")
+        }
+
+        val zipA = createZip {
+            addDirectory("dir-a/")
+            addFile("dir-a/file-c.txt", "Hello C!")
+            addZipFile("dir-a/zip-b.zip", zipB)
+            addZipFile("dir-a/zip-f.zip", zipF)
+        }
+
+        val zipG = createZip {
+            addDirectory("dir-g/")
+        }
+
+        return createZip {
+            addFile("file-a.txt", "Hello A!")
+            addFile("file-b.txt", "Hello B!")
+            addZipFile("zip-a.zip", zipA)
+            addZipFile("zip-g.zip", zipG)
+        }
+    }
+
+    private fun createZip(builder: ZipBuilder.() -> Unit): ByteArray {
+        val output = ByteArrayOutputStream()
+        ZipOutputStream(output).use { zipOut ->
+            ZipBuilder(zipOut).builder()
+        }
+        return output.toByteArray()
+    }
+
+    private class ZipBuilder(private val zipOut: ZipOutputStream) {
+        fun addFile(name: String, content: String) {
+            zipOut.putNextEntry(ZipEntry(name))
+            zipOut.write(content.toByteArray(StandardCharsets.UTF_8))
+            zipOut.closeEntry()
+        }
+
+        fun addZipFile(name: String, content: ByteArray) {
+            zipOut.putNextEntry(ZipEntry(name))
+            zipOut.write(content)
+            zipOut.closeEntry()
+        }
+
+        fun addDirectory(name: String) {
+            zipOut.putNextEntry(ZipEntry(name))
+            zipOut.closeEntry()
+        }
+    }
 }
